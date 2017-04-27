327e4fb75962d72ddd2f3926fd7b2c1934740260
Kui Xu <xukui.cs@gmail.com>
2017-04-28 02:59:43 +0800

    add two clustering metrics

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.misc import logsumexp
+from sklearn import metrics
 
 def loadData(libfilepath, vocfilepath):
     print "Loading data......"
@@ -56,19 +57,20 @@ def MultinomialMixtureEM(X, K, P_Cd=None, eps=0.00001, maxiter=1000):
     convergince = np.finfo('float').max
     i = 0
     while i < maxiter and convergince > eps:
+        # E-step
         theta = np.sum(P_Cd, axis=0) / D
-        phi = (X.T.dot(P_Cd) / np.sum(P_Cd.T * Nd, axis=1)).T
-        L = np.log(theta) + X.dot(np.log(phi.T))
-        
-        P_Cd_new = np.exp((L.T - logsumexp(L, axis=1)).T)
+        U = (X.T.dot(P_Cd) / np.sum(P_Cd.T * Nd, axis=1)).T
+        L = np.log(theta) + X.dot(np.log(U.T))
+        # M-step
+        P_Cd_new = np.exp((L.T - logsumexp(L, axis=1)).T) +0.000000000001
         convergince = np.max(np.abs(P_Cd_new-P_Cd))
         P_Cd = P_Cd_new
         i += 1
 
-    return phi, P_Cd
+    return U, P_Cd
 
-def printResults(phi, P_Cd, K,NumMostFrequent, X, wordDict):
-    (D,W)=X.shape
+def printResults(U, NumMostFrequent, wordDict):
+    (K,W)=U.shape
     #NumMostFrequent =5
     word = [[] for i in range(K)]
     #f = file('./nips/output.txt', 'w+')
@@ -78,7 +80,7 @@ def printResults(phi, P_Cd, K,NumMostFrequent, X, wordDict):
         #print>>f, "The cluster %d\'s ratio is %f, most-frequent %d words are: " % (i+1, self.PI[i], num)
         list = []
         for j in range(W):
-            list.append((phi[i,j],j))
+            list.append((U[i,j],j))
         list.sort(key=lambda x:x[0], reverse=True)
         for j in range(NumMostFrequent):
             word[i].append(wordDict[list[j][1]])
@@ -102,56 +104,32 @@ def printResults(phi, P_Cd, K,NumMostFrequent, X, wordDict):
     sim = sim/count
     return sim
 
-
-
-Kset = [5,10,20,30]
-index = 0
-NumMostFrequent = 5
-bestKsim= 1
-for i in range(len(Kset)):
-    K = Kset[i]
+if __name__ == '__main__':
     
-    
+
+    Kset = [5,10,20,30]
+    index = 0
+    nWord = 5
+    bestKsim= 1
     X, wordDict = loadData("./nips/nips.libsvm", "./nips/nips.vocab")
-    #Dataset = Input.Dataset(voc_path=voc_file, lib_path=lib_file)
-    #print "Dataset done......"
-    #X= np.asarray(Dataset.x_)
-    (D,W)=X.shape
-    phi, P_Cd = MultinomialMixtureEM(X,K)
-    print phi.shape
-    print P_Cd.shape
-    sim = printResults(phi, P_Cd, K, NumMostFrequent, X, wordDict)
-    if bestKsim > sim:
-        bestKsim= sim
-        index = i
-    print "Average similarity is %f " %(sim)
-    #print "Building model......"
-    #MoM = model.model(cluster_num, Dataset.num_lib, Dataset.voc_size)
-    #print "Model done......"
+    for i in range(len(Kset)):
+        K = Kset[i]
+        
+        U, P_Cd = MultinomialMixtureEM(X,K)
+        #print U.shape
+        #print P_Cd.shape
+        labels=np.argmax(P_Cd,axis=1)
+        #http://scikit-learn.org/stable/modules/clustering.html#clustering
+        a=metrics.silhouette_score(X, labels, metric='euclidean')
+        b=metrics.calinski_harabaz_score(X, labels) 
 
-    #MoM.train(x_=np.array(Dataset.x_, dtype='float'), iter_num=20)
+        sim = printResults(U, nWord, wordDict)
+        if bestKsim > sim:
+            bestKsim= sim
+            index = i
+        print "Average similarity is %f " %(sim)
+        print "Silhouette Coefficient is %f " %(a)
+        print "Calinski-Harabaz Index is %f \n" %(b)
 
-    #MoM.output(5, dataset=Dataset)
-
-    #if MoM.sim < mn:
-        #mn = MoM.sim
-        #index = i
-print ""
-print "The best K is: %d " %(Kset[index])
-
-
-
-# voc_file = "./nips/nips.vocab"
-# lib_file = "./nips/nips.libsvm"
-#
-# # file = open(lib_file)
-# print "Geting dataset......"
-# Dataset = Input.Dataset(voc_path=voc_file, lib_path=lib_file)
-# print "Dataset done......"
-#
-# print "Building model......"
-# MoM = model.model(cluster_num, Dataset.num_lib, Dataset.voc_size)
-# print "Model done......"
-#
-# MoM.train(x_=np.array(Dataset.x_, dtype='float'), iter_num=20)
-# MoM.output(5, dataset=Dataset)
+    print ""
+    print "The best K is: %d \n" %(Kset[index])
